aa07b2f79085ea483ac554508b3df378b5317057
Gnasche <phuoc.workspace@gmail.com>
2024-10-12 21:28:13 +0700

    update parallel mode

--- config/appConfig.ts
+++ config/appConfig.ts
@@ -2,8 +2,8 @@ export const appConfig = {
     baseUrl: 'https://demoqa.com',
     user: 'usergs25',
     password: 'Password@123',
-    enableParallel: false, // Enable or disable parallel running
-    maxWorkers: 2, // Required if enableParallel is true
+    enableParallel: true, // Enable or disable parallel running
+    maxWorkers: 2, // Required if enableParallel is true -- 11 is on config and default is parallel -- npx jest --showConfig
   };
 
   //user id:  8504e8b7-4c83-47ad-b97b-48144366cbb2
--- runner/runAllTest.ts
+++ runner/runAllTest.ts
@@ -1,6 +1,16 @@
 import { exec } from 'child_process';
+import { appConfig } from '../config/appConfig';
 process.env.GENERATE_HTML_REPORT = 'true';
-exec('npx jest --detectOpenHandles', (error, stdout, stderr) => {
+
+var command = 'npx jest --detectOpenHandles';
+if (appConfig.enableParallel === true){
+    command = `npx jest --detectOpenHandles --maxWorkers=${appConfig.maxWorkers}`;
+}
+else{
+    command = 'npx jest --detectOpenHandles --runInBand';
+}
+
+exec(command, (error, stdout, stderr) => {
     if (error) {
         console.error(`Error running tests: ${stderr}`);
         process.exit(1);
@@ -9,3 +19,12 @@ exec('npx jest --detectOpenHandles', (error, stdout, stderr) => {
 });
 
 //ts-node .\runner\runAllTest.ts
+
+// # Run with default parallelism
+// npx jest
+
+// # Run with 4 workers
+// npx jest --maxWorkers=4
+
+// # Run tests sequentially (no parallel execution)
+// npx jest --runInBand
